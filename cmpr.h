be17b4c3f8bf4ba6b25bb9f687c0b3bb925cfd23
saranomy <anti7gn@gmail.com>
2020-12-09 16:17:46 -0800

    Modify the assignment

--- cmpr.h
+++ cmpr.h
@@ -4,3 +4,6 @@ float unit_p(float price, long quantity);
 // return 1 if unit price of the first one is cheaper
 // -1 if more expensive, or 0 if same
 int cmpr(float price1, long quantity1, float price2, long quantity2);
+
+// same as cmpr, but comparing between 2 unit prices
+int cmpr_u(float unit_p1, float unit_p2);
